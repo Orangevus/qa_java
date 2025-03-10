64d1ab1a8e204d4d18cf3909b81552456657ff5f
Aleksandr Belimov <vontas@mail.ru>
2025-03-10 19:15:17 +0300

    Sprint_6  FIX

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -4,9 +4,20 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+
 
 public class FelineTest {
     Feline feline = new Feline();
+    @Spy
+    private Feline spyFeline;
+    @Test
+    public void getKittensWithNoParametersCallsGetKittens() {
+        spyFeline = Mockito.spy(feline);
+        spyFeline.getKittens();
+        Mockito.verify(spyFeline).getKittens(1);
+    }
 
     @Test
     public void eatMeatForFelineReturnPredatorEat() throws Exception {
